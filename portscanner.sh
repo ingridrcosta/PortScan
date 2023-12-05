732352341c65b7f0859d71de5b01e6cc1f43e21b
Ingrid Ruth <84602805+ingridrcosta@users.noreply.github.com>
2023-12-04 22:07:31 -0300

    Update portscanner.sh
    Atualização para o NetCat fazer leituras de IP de máscaras /24 e /30, vai ser adicionado mais mascaras no decorrer do tempo...
    
    
    Att.: Ingrid

--- portscanner.sh
+++ portscanner.sh
@@ -15,59 +15,76 @@ if [ ! -x "$0" ]; then
         chmod +x "$0"
 
         if [ $? -ne 0 ]; then
-                echo "Erro ao tornar o script executavel" | awk ' {print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogs"
+                echo "Erro ao tornar o script executavel" | awk ' {print strftime("%d/%m/%Y %H:%M"), $0 }' > "$GeradorLogsNC"
                 exit 1 
         else
-                echo "Script executado com sucesso." | awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogs"       
+                echo "Script executado com sucesso." | awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' > "$GeradorLogsNC"       
         fi
 fi
 
 
-Diretorio="SEU DIRETORIO" #EX.: "/home/user/Downloads"
-GeradorLogs="SEU DIRETORIO/log.txt" #EX.: "/home/user/DownLoads/Log/log.txt" Nota: Alguns diretorios podem dar erro de "Diretorio nao existente", recomendado alterar o diretorio para um caminho mais curto, evite diretorios ambiguos. 
+Diretorio="/home/lain/PUC/NetCatScanner/scan/" 
+GeradorLogsNC="/home/lain/PUC/NetCatScanner/scan/log.txt"
 
-if [ ! -d "$diretorio" ];then
+exec 3>&1 4>&2 
+exec > "$GeradorLogsNC" 2>&1 
+
+
+echo "Starting script at $(date)" >&3
+
+
+if [[ ! -d $Diretorio ]];then
         mkdir -p "$Diretorio"
-        mkdir -p "$(dirname "$GeradorLogs")"
+        mkdir -p "$(dirname "$GeradorLogsNC")"
+        echo "Diretorio criado: $Diretorio" | awk ' {print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogsNC"
+    else   
+        echo "Diretorio Existente" "$Diretorio" | awk ' {print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogsNC"    
+
+
 fi
 
-if ! command -v nc &>/dev/null; then
+if ! command -v nc &>>/dev/null; then
 
         sudo apt install netcat-openbsd -y
-        echo "NetCat instalado." | awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' >"$GeradorLogs"
+        echo "NetCat instalado." | awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogsNC"
 
 fi
-
-BlocoDeIP="$1"
-port_inicial="$2"
-port_final="$3"
-range="$port_inicial-$port_final"
-
 if [ $# -eq 0 ]; then
-        echo " Insira Bloco IP /24 +  Range de portas validos!"| awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' > "$GeradorLogs"
+        echo " Insira Bloco IP /24 +  Range de portas validos!"| awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogsNC"
         exit 1
-fi  
+fi 
+BlocoDeIP="$1"
+Mascara="$2"
+Porta_Inicial="$3"
+Porta_Final="$4"
+range="$Porta_inicial-$Porta_final"
 
-if [[ ! $BlocoDeIP =~ ^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.0\/24$ ]] ;
-then 
-        echo " Bloco IP invalido : $BlocoDeIP (exemplo: 192.168.1.0/24)"| awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' > "$GeradorLogs"
-        exit 1
-fi
 
-if [ -n "$BlocoDeIP" ] &&  [[ ! $range =~ ^[0-9]{1,5}-[0-9]{1,5}$ ]] ;then
-        echo " Range de portas invalido / Nao especificado (Valido 1 a 65535)" | awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' > "$GeradorLogs"
-        exit 1
+ip_saida=$(echo "$BlocoDeIP"/"$Mascara" | awk -F '[.]' '{print $1"."$2"."$3"."}')
 
-fi
+echo "" >> "$GeradorLogsNC"
 
-ip_saida=$(echo "$BlocoDeIP" | awk -F '[.]' '{print $1"."$2"."$3"."}')
-
-echo "" > "$GeradorLogs"
-
-for octeto in $(seq 20 30); do 
+if [[ $Mascara == 24 ]]; then
+for octeto in $(seq 1 255); do 
         bloco_ip="$ip_saida$octeto"
-        for port in $(seq $port_inicial $port_final);do 
-                nc -zv "$bloco_ip" "$port" 2>&1 | awk '/succeeded/{print strftime("%d/%m/%Y %H:%M"),$3,$4,$5,$6}' >> "$GeradorLogs"
+        for mascara in $(seq $Mascara 32);do
+                for port in $(seq $Porta_Inicial $Porta_Final);do
+                        nc -zv $bloco_ip $port 2>&1 | awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogsNC"
+                done       
         done
 done
 
+else 
+if [[ $Mascara == 30 ]]; then
+for octeto in $(seq 1 3); do 
+        bloco_ip="$ip_saida$octeto"
+        for mascara in $(seq $Mascara 32);do
+                for port in $(seq $Porta_Inicial $Porta_Final);do
+                        nc -zv $bloco_ip $port 2>&1 | awk '{print strftime("%d/%m/%Y %H:%M"), $0 }' >> "$GeradorLogsNC"
+                done       
+        done
+done
+fi
+fi
+
+exec 1>&3 2>&4 
