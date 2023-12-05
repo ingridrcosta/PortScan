d33efa490f53d2daf4d0242b5b1365c5c9017f64
Ingrid Ruth <84602805+ingridrcosta@users.noreply.github.com>
2023-12-04 22:09:23 -0300

    Update Program.cs
    Correção para leituras de máscaras de sub rede

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/scan/{iPRede}/{portaInicial}/{portaFinal}", async context  =>
+app.MapGet("/scan/{ipRede}/{mascara}/{inicio}/{fim}", async context =>
 {
-    var iPRede = context.Request.RouteValues["iPRede"];
-    var portaInicial = context.Request.RouteValues["portaInicial"];
-    var portaFinal = context.Request.RouteValues["portaFinal"];
+    var ipRede = context.Request.RouteValues["ipRede"];
+    var mascaraIP = context.Request.RouteValues["mascara"];
+    var portaInicio = context.Request.RouteValues["inicio"];
+    var portaFim = context.Request.RouteValues["fim"];
+    
 
     System.Diagnostics.ProcessStartInfo process = new System.Diagnostics.ProcessStartInfo();
     process.UseShellExecute = false;
     process.WorkingDirectory = "/bin";
     process.FileName = "bash";
-    process.Arguments = $"$HOME/ScannerPort/portscanner.sh {iPRede} {portaInicial} {portaFinal}";
+    process.Arguments = $"/home/lain/PUC/NetCatScanner/portscanner.sh {ipRede} {mascaraIP} {portaInicio} {portaFim}";
     process.RedirectStandardOutput = true;
 
     using (System.Diagnostics.Process cmd = System.Diagnostics.Process.Start(process))
@@ -19,10 +21,10 @@ app.MapGet("/scan/{iPRede}/{portaInicial}/{portaFinal}", async context  =>
         cmd.WaitForExit();
     }
 
-    string logContent = await System.IO.File.ReadAllTextAsync("$HOME/ScannerPort/Logs/log.txt");
+    string logContent = await System.IO.File.ReadAllTextAsync("/home/lain/PUC/NetCatScanner/scan/log.txt");
 
-    context.Response.ContentType = "text/plain";
-    await context.Response.WriteAsync($"IP da Rede: {iPRede}. Portas de: {portaInicial}. Ate: {portaFinal}.\n");
+    context.Response.ContentType = "text/plain"; 
+    await context.Response.WriteAsync($"IP da rede: {ipRede}/{mascaraIP} e {portaInicio} e {portaFim}.\n");
     await context.Response.WriteAsync(logContent);
 });
 
